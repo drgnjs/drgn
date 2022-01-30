aa9cb8637b765c241cdd19474a819bcca444947e
Azury <azury.wtf@gmail.com>
2022-01-30 15:57:56 +0100

    added webpack rules

---
+++ webpack.rules.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    test: /native_modules\/.+\.node$/,
+    use: 'node-loader'
+  },
+  {
+    test: /\.s[ac]ss$/i,
+    use: [
+      'style-loader',
+      'css-loader',
+      'sass-loader'
+    ]
+  },
+  {
+    test: /\.(m?js|node)$/,
+    parser: { amd: false },
+    use: {
+      loader: '@vercel/webpack-asset-relocator-loader',
+      options: {
+        outputAssetBase: 'native_modules'
+      }
+    }
+  },
+  {
+    test: /\.tsx?$/,
+    exclude: /(node_modules|\.webpack)/,
+    use: {
+      loader: 'ts-loader',
+      options: {
+        transpileOnly: true
+      }
+    }
+  }
+]

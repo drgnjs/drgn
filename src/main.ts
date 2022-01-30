a3d99807aca6ef2e0b05064f6a3eda54da6357e8
Azury <azury.wtf@gmail.com>
2022-01-30 15:54:10 +0100

    added main file

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { app, BrowserWindow } from 'electron'
+import { join } from 'path'
+declare const MAIN_WINDOW_WEBPACK_ENTRY: string
+
+if (require('electron-squirrel-startup')) app.quit()
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+    center: true,
+    frame: false,
+    title: 'drgn'
+  })
+
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
+  mainWindow.removeMenu()
+  mainWindow.setIcon(join(__dirname, '../../src/assets/icon.png'))
+
+  // mainWindow.webContents.openDevTools()
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})

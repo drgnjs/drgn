25f7faaba545f01811c6b2cc9ce13235697f1a39
Azury <azury.wtf@gmail.com>
2022-02-01 20:21:04 +0100

    added option to make popup uncloseable

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -3,19 +3,21 @@ import Icon from './Icon'
 import styles from '../styles/Popup.module.scss'
 import Portal from './Portal'
 
-const Popup = ({ content, children, hideIcon }: {
+const Popup = ({ content, children, hideIcon, isVisible, isCloseable }: {
   content: ReactNode,
   children: ReactNode,
-  hideIcon?: boolean
+  hideIcon?: boolean,
+  isVisible?: boolean,
+  isCloseable?: boolean
 }) => {
-  const [visible, setVisibility] = useState(false)
+  const [visible, setVisibility] = useState(isVisible === true)
 
   return (
     <>
       <Portal>
-        <div className={visible ? styles.overlayActive : styles.overlay} onClick={() => setVisibility(false)}></div>
+        <div className={visible ? styles.overlayActive : styles.overlay} onClick={isCloseable ? () => setVisibility(false) : null}></div>
           <div className={visible ? styles.popupActive : styles.popup}>
-          {!hideIcon && <Icon className={styles.icon} name='close' onClick={() => setVisibility(false)} />}
+          {(!hideIcon && isCloseable) && <Icon className={styles.icon} name='close' onClick={() => setVisibility(false)} />}
           {content}
         </div>
       </Portal>

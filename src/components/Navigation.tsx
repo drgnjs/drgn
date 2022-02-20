5c279eec50f158f14599a94912ff4b3e7e3971d7
Azury <azury.wtf@gmail.com>
2022-02-20 12:45:38 +0100

    added version and edited icons

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,15 +3,13 @@ import { NavLink } from 'react-router-dom'
 import styles from '../styles/Navigation.module.scss'
 import Icon from './Icon'
 import Tip from './Tip'
-import { get } from '../modules/cache'
 import Popup from './Popup'
-
-const allowedProtocols = ['FTP', 'SFTP', 'S3']
+import { protocols } from '../config.json'
+import { version } from '../../package.json'
 
 const Navigation = () => {
   const exists = false
   const [protocol, setProtocol] = useState<string>(null)
-
 
   return (
     <nav className={styles.navigation}>
@@ -25,13 +23,29 @@ const Navigation = () => {
           <span className={styles.separator}></span>
         </>
       }
+      <Tip content='Files' placement='right'>
+        <NavLink className={({ isActive }) => isActive ? styles.activeItem : styles.item} to='/files'>
+          <Icon name='folder_open' />
+        </NavLink>
+      </Tip>
+      <Tip content='Servers' placement='right'>
+        <NavLink className={({ isActive }) => isActive ? styles.activeItem : styles.item} to='/servers'>
+          <Icon name='dns' />
+        </NavLink>
+      </Tip>
+      <Tip content='New Server' placement='right'>
+        <NavLink className={({ isActive }) => isActive ? styles.activeItem : styles.item} to='/'>
+          <Icon name='home' />
+        </NavLink>
+      </Tip>
+      <span className={styles.separator}></span>
       <Popup
         content={
           <div className={styles.addNewServer}>
             <h1>Connect to Server</h1>
             
             <label>
-              <p><b>Protocol</b> {protocol && !allowedProtocols.includes(protocol) && <>- <b className={styles.inputError}>Must be either FTP, SFTP, or S3!</b></>}</p>
+              <p><b>Protocol</b> {protocol && !protocols.includes(protocol) && <>- <b className={styles.inputError}>Must be either FTP, SFTP, or S3!</b></>}</p>
               <input type='text' placeholder='e.g. SFTP' onChange={e => setProtocol(e.target.value.toUpperCase())} />
             </label>
             <div className={styles.specialRow}>
@@ -56,14 +70,17 @@ const Navigation = () => {
             </div>
           </div>
         }
-        isVisible
+        //isVisible
+        isCloseable
       >
         <Tip content='New Server' placement='right'>
-          <NavLink className={({ isActive }) => isActive ? styles.activeItem : styles.item} to='/'>
+          <div className={styles.item}>
             <Icon name='dashboard_customize' />
-          </NavLink>
+          </div>
         </Tip>
       </Popup>
+
+      <p className={styles.version}>v{version}</p>
     </nav>
   )
 }

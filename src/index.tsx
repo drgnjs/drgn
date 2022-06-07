4a7c57ec384ca97aaf898876f87ad08c42dc2baa
Azury <azury.wtf@gmail.com>
2022-06-07 20:08:24 +0200

    added imports for fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/global.scss'
 import styles from './styles/App.module.scss'
+import '@fontsource/poppins'
+import '@fontsource/poppins/500.css'
+import '@fontsource/poppins/600.css'
+import '@fontsource/poppins/700.css'
+import '@fontsource/poppins/800.css'
+import '@fontsource/poppins/900.css'
+import '@fontsource/poppins/400-italic.css'
+import '@fortawesome/fontawesome-free/css/brands.css'
+import 'material-symbols/outlined.css'
+import 'material-symbols/rounded.css'
+import './styles/global.scss'
 
 const App = () => {
   return (

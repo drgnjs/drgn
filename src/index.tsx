2870d1243554b102bbdafc887e910521fe6d8852
Azury <azury.wtf@gmail.com>
2022-06-07 18:35:15 +0200

    edited other static files (test purposes only)

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/global.scss'
+import styles from './styles/App.module.scss'
 
 const App = () => {
   return (
-    <div style={{ position: 'relative' }}>
-      
+    <div className={styles.lol}>
+      <h1>This a test.</h1>
     </div>
   )
 }

6608043cc9a486f53180cb8432f9370cb6ec0f59
Azury <azury.wtf@gmail.com>
2022-06-08 16:20:34 +0200

    yet another test run

--- scripts/bumpVersion.js
+++ scripts/bumpVersion.js
@@ -2,6 +2,6 @@ import { readFile, writeFile } from 'fs/promises'
 
 const tauriJson = JSON.parse(await readFile('src-tauri/tauri.conf.json', { encoding: 'utf-8' }))
 
-tauriJson.package.version = (await readFile('version.txt', { encoding: 'utf-8' })).replace('v', '').replace('\n', '')
+tauriJson.package.version = (await readFile('version.txt', { encoding: 'utf-8' })).replace('v', '')
 
 await writeFile('src-tauri/tauri.conf.json', JSON.stringify(tauriJson, null, 2) + '\n')

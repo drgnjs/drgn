b01e738a712a96e09dfed8219a5859392e892108
Azury <azury.wtf@gmail.com>
2022-06-08 13:35:24 +0200

    another test

--- scripts/setVersion.js
+++ scripts/setVersion.js
@@ -3,5 +3,5 @@ import { readFile, writeFile } from 'fs/promises'
 const tauriJson = JSON.parse(await readFile('src-tauri/tauri.conf.json', { encoding: 'utf-8' }))
 
 tauriJson.package.version = (await readFile('version.txt', { encoding: 'utf-8' })).replace('v', '')
-
+console.log(tauriJson.package.version)
 await writeFile('src-tauri/tauri.conf.json', JSON.stringify(tauriJson, null, 2) + '\n')

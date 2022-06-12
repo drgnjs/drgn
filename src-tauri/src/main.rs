8360c40dd9483cc0c780a6d422ca58c6fb2b163a
Azury <azury.wtf@gmail.com>
2022-06-12 23:40:14 +0200

    added `window-shadows` plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,18 @@
-#![cfg_attr(
+use tauri::Manager;
+use window_shadows::set_shadow;
+
+#[cfg_attr(
   all(not(debug_assertions), target_os = "windows"),
   windows_subsystem = "windows"
 )]
 
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+      let win = app.get_window("main").unwrap();
+      let _ = set_shadow(&win, true); // Using `let _ =` to ignore warning of unused Result. unwrapping is not safe because this function is not supported on Linux.
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }

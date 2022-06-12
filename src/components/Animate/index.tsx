bdc2ca19a596725c018d6966298e745cf52197f4
Azury <azury.wtf@gmail.com>
2022-06-12 02:40:00 +0200

    added `Animate` component

---
+++ src/components/Animate/index.tsx
@@ -0,0 +1,36 @@
+import styles from './Animate.module.scss'
+import { useState, useRef, useEffect, ReactNode } from 'react'
+
+const Animate = ({ children, animation, delay, className, ...rest }: {
+  children: ReactNode,
+  animation: 'fadeUp' | 'fadeDown' | 'fadeRight' | 'fadeLeft' | 'fadeUpRight' | 'fadeUpLeft' | 'zoomIn' | 'zoomInUp' | 'zoomInDown' | 'zoomInRight' | 'zoomInLeft' | 'zoomOut' | 'zoomOutUp' | 'zoomOutDown' | 'zoomOutRight' | 'zoomOutLeft' | 'flipLeft' | 'flipRight' | 'flipUp' | 'flipDown',
+  className?: string,
+  delay?: number,
+  [key: string]: any
+}) => {
+  const [isVisible, setVisible] = useState(false)
+  const domRef = useRef()
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => setVisible(entry.isIntersecting))
+    })
+
+    // @ts-ignore
+    observer.observe(domRef.current)
+  }, [])
+
+  return (
+    <div
+      style={delay ? { transitionDelay: `${delay}s` } : undefined}
+      className={`${styles[animation]}${isVisible ? ` ${styles.isVisible}` : ''}${className ? ` ${className}` : ''}`}
+      // @ts-ignore
+      ref={domRef}
+      {...rest}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default Animate

6542a69f3548f16267ce9b32cb4d4d7550b7326e
Azury <azury.wtf@gmail.com>
2022-06-12 23:44:48 +0200

    added `Avatar` component

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,18 @@
+import { createAvatar } from '@dicebear/avatars'
+import * as style from '@dicebear/avatars-identicon-sprites'
+import { ImgHTMLAttributes } from 'react'
+
+interface Attributes extends ImgHTMLAttributes<HTMLImageElement> {
+  seed: string
+}
+
+const Avatar = ({ seed, ...rest }: Attributes) => {
+  const dataUri = createAvatar(style, {
+    seed,
+    dataUri: true
+  })
+
+  return <img src={dataUri} alt='avatar' {...rest} />
+}
+
+export default Avatar

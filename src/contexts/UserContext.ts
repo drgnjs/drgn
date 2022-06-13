5c3e5abf588557489096942a7f98e051317951fa
Azury <azury.wtf@gmail.com>
2022-06-13 19:24:08 +0200

    fixed types of user context
    just a workaround for now, disabled type checking

--- src/contexts/UserContext.ts
+++ src/contexts/UserContext.ts
@@ -2,6 +2,6 @@ import { createContext, Dispatch, SetStateAction } from 'react'
 import { User } from '../types'
 
 // @ts-ignore
-const UserContext = createContext<{ user: User | null, setUser: Dispatch<SetStateAction<User | null>>, token: string | null, setToken: (token: string) => void }>(null)
+const UserContext = createContext<{ user: User | null, setUser: Dispatch<SetStateAction<User | null>>, token: string | null, setToken: (token: string | null) => void }>(null)
 
 export default UserContext

bd99701d37842881a64294fc94bbb58914f3b822
Azury <azury.wtf@gmail.com>
2022-06-15 01:15:09 +0200

    fixed createUpdate script

--- scripts/createUpdate.js
+++ scripts/createUpdate.js
@@ -1,8 +1,5 @@
 import { readFile, writeFile } from 'fs/promises'
 
-// get latest release
-const latestRelease = await (await fetch('https://api.github.com/repos/drgnjs/drgn/releases/latest')).json()
-
 // get json file
 const releaseJson = JSON.parse(await readFile('release.json', { encoding: 'utf-8' }))
 
@@ -12,19 +9,19 @@ releaseJson.notes = `Read our changelog to learn more: https://drgnjs.com/change
 releaseJson.pub_date = process.env.PUBLISHED_AT.replaceAll('\"', '')
 
 // update signatures
-const macSignature = await (await fetch(`https://github.com/drgnjs/drgn/releases/download/${latestRelease.tag_name}/drgn.app.tar.gz.sig`)).text()
+const macSignature = await (await fetch(`https://github.com/drgnjs/drgn/releases/download/${process.env.VERSION}/drgn.app.tar.gz.sig`)).text()
 releaseJson.platforms.darwin.signature = macSignature
 
-const linuxSignature = await (await fetch(`https://github.com/drgnjs/drgn/releases/download/${latestRelease.tag_name}/drgn_0.0.0_amd64.AppImage.tar.gz.sig`)).text()
+const linuxSignature = await (await fetch(`https://github.com/drgnjs/drgn/releases/download/${process.env.VERSION}/drgn_${process.env.VERSION.replace('v', '')}_amd64.AppImage.tar.gz.sig`)).text()
 releaseJson.platforms.linux.signature = linuxSignature
 
-const windowsSignature = await (await fetch(`https://github.com/drgnjs/drgn/releases/download/${latestRelease.tag_name}/drgn_0.0.0_x64_en-US.msi.zip.sig`)).text()
+const windowsSignature = await (await fetch(`https://github.com/drgnjs/drgn/releases/download/${process.env.VERSION}/drgn_${process.env.VERSION.replace('v', '')}_x64_en-US.msi.zip.sig`)).text()
 releaseJson.platforms.win64.signature = windowsSignature
 
 // update download urls
-releaseJson.platforms.darwin.url = `https://github.com/drgnjs/drgn/releases/download/${latestRelease.tag_name}/drgn.app.tar.gz`
-releaseJson.platforms.linux.url = `https://github.com/drgnjs/drgn/releases/download/${latestRelease.tag_name}/drgn_0.0.0_amd64.AppImage.tar.gz`
-releaseJson.platforms.win64.url = `https://github.com/drgnjs/drgn/releases/download/${latestRelease.tag_name}/drgn_0.0.0_x64_en-US.msi.zip`
+releaseJson.platforms.darwin.url = `https://github.com/drgnjs/drgn/releases/download/${process.env.VERSION}/drgn.app.tar.gz`
+releaseJson.platforms.linux.url = `https://github.com/drgnjs/drgn/releases/download/${process.env.VERSION}/drgn_${process.env.VERSION.replace('v', '')}_amd64.AppImage.tar.gz`
+releaseJson.platforms.win64.url = `https://github.com/drgnjs/drgn/releases/download/${process.env.VERSION}/drgn_${process.env.VERSION.replace('v', '')}_x64_en-US.msi.zip`
 
 // update json file
 await writeFile('release.json', JSON.stringify(releaseJson, null, 2) + '\n')

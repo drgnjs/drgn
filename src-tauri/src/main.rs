ae0392fa62f43eeaa681dd63ed5cb0f3f4253888
Azury <azury.wtf@gmail.com>
2022-06-14 18:25:18 +0200

    bumped version to `v1.0.0`

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,8 +9,11 @@ use window_shadows::set_shadow;
 fn main() {
   tauri::Builder::default()
     .setup(|app| {
-      let win = app.get_window("main").unwrap();
-      let _ = set_shadow(&win, true); // Using `let _ =` to ignore warning of unused Result. unwrapping is not safe because this function is not supported on Linux.
+      #[cfg(not(target_os = "linux"))]
+      {
+        let win = app.get_window("main").unwrap();
+        let _ = set_shadow(&win, true); // Using `let _ =` to ignore warning of unused Result. unwrapping is not safe because this function is not supported on Linux.
+      }
       Ok(())
     })
     .run(tauri::generate_context!())

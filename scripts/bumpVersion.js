c9527f41c2eb2ac5480f6155920e3b8d674033c0
Azury <azury.wtf@gmail.com>
2022-06-14 19:17:19 +0200

    removed bumpVersion script

--- scripts/bumpVersion.js
+++
@@ -1,7 +0,0 @@
-import { readFile, writeFile } from 'fs/promises'
-
-const tauriJson = JSON.parse(await readFile('src-tauri/tauri.conf.json', { encoding: 'utf-8' }))
-
-tauriJson.package.version = (await readFile('version.txt', { encoding: 'utf-8' })).replace('v', '').replace('\n', '')
-
-await writeFile('src-tauri/tauri.conf.json', JSON.stringify(tauriJson, null, 2) + '\n')

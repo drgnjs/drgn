68b4044845baf18079aef3cef32c0978d223d00d
Azury <azury.wtf@gmail.com>
2022-06-20 16:08:22 +0200

    fixed type issues in `Tip` component

--- src/components/Tip.tsx
+++ src/components/Tip.tsx
@@ -1,25 +1,23 @@
 import Tippy from '@tippyjs/react'
+import { useRef, useState, useEffect } from 'react'
 import type { TippyProps } from '@tippyjs/react'
-import { ReactNode, useRef, useState, useEffect } from 'react'
 
-interface TipConfig extends TippyProps {
-  children: any,
-  content: string | ReactNode,
-  [any: string]: any
-}
-
-const Tip = ({ children, content, ...rest }: TipConfig) => {
-  const spanRef = useRef(null)
+const Tip = ({ children, content, ...rest }: TippyProps) => {
+  const spanRef = useRef<HTMLSpanElement>(null)
   const [childRef, setChildRef] = useState(null)
 
   useEffect(() => {
-    if (spanRef.current) setChildRef(spanRef.current.previousElementSibling)
+    if (spanRef.current)
+      // @ts-ignore
+      setChildRef(spanRef.current.previousElementSibling)
+
     return () => setChildRef(null)
   }, [])
 
   return (
     <>
       {children}
+
       {childRef ? (
         <Tippy animation='scale-subtle' arrow={false} content={content} {...rest} reference={childRef} />
       ) : (

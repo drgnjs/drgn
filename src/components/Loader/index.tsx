976ee6347f96f27c435f4bf9f84ac6a3dd5741f0
Azury <azury.wtf@gmail.com>
2022-06-24 21:48:48 +0200

    added `Loader` component

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,22 @@
+import styles from './styles.module.sass'
+
+const Loader = () => {
+  return (
+    <div className={styles.circle}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Loader
